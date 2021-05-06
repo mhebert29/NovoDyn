72faa23e785a20e8f3df761b7dcdb19eafca6e38
Dawson Flatley <dflatley@nd.edu>
2021-05-06 18:42:42 -0400

    Multiple tags input implemented

--- parsing.py
+++ parsing.py
@@ -4,25 +4,34 @@ import os
 import json
 import pathlib
 from pathlib import Path
-def findtags(folder,tag):
+
+
+def findtags(folder,tags):
+	outlist = []
+	filteredtags = {}
 	for filename in os.listdir(folder):
+		for tag in tags:
+			print(f"setting {tag} to false")
+			filteredtags[tag] = False;
 		mypath = folder + "/" + filename
+		print(f"path: {mypath}")
 		with open(mypath) as f:
 			data = json.load(f)
 			for region in data["regions"]:
-				if tag in region["tags"]:
-					return data["asset"]["path"]
+				for intag in region["tags"]:
+					if intag in filteredtags:
+						filteredtags[intag] = True
+		if all(filteredtags.values()):
+			outlist.append(filename)
+
+	return outlist
 			
 if __name__ == "__main__":
-	istherepath = False
-	for arg in sys.argv[1:]:
-		if not istherepath:
-			path = arg
-			istherepath = True
-		else:
-			tag = arg
-			print(findtags(path, tag))
-			istherepath = False
+	if len(sys.argv) < 3 or len(sys.argv) >3:
+		print("Usage:    [program] [json directory] [list of tags to search for]")
+		exit()
+	findtags(sys.argv[1], sys.argv[2])
+		
 			
 			
 			
