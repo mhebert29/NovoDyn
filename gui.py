eb38ee4b0bca1b9f14a4d58e6bd10a523b2c1054
Madeline Hebert <69490091+mhebert29@users.noreply.github.com>
2021-05-06 19:29:41 -0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -6,51 +6,315 @@ master=tkinter.Tk()
 
 opWindow = Toplevel(master)
 opWindow.title("Make a pick!")
-opWindow.geometry("250x250")
+opWindow.geometry("200x500")
+
+## FLAGS TO INSERT AS OPTIONS: 1-32, cavity, crowns, restorations, enamel ##
 
 Checkbutton1 = tkinter.IntVar()  
 Checkbutton2 = tkinter.IntVar()  
 Checkbutton3 = tkinter.IntVar() 
+Checkbutton4 = tkinter.IntVar()  
+Checkbutton5 = tkinter.IntVar()  
+Checkbutton6 = tkinter.IntVar()  
+Checkbutton7 = tkinter.IntVar()  
+Checkbutton8 = tkinter.IntVar() 
+Checkbutton9 = tkinter.IntVar()  
+Checkbutton10 = tkinter.IntVar()  
 Checkbutton11 = tkinter.IntVar()  
 Checkbutton12 = tkinter.IntVar()  
+Checkbutton13 = tkinter.IntVar() 
+Checkbutton14 = tkinter.IntVar()  
+Checkbutton15 = tkinter.IntVar()  
+Checkbutton16 = tkinter.IntVar()  
+Checkbutton17 = tkinter.IntVar()  
+Checkbutton18 = tkinter.IntVar() 
+Checkbutton19 = tkinter.IntVar()  
+Checkbutton20 = tkinter.IntVar()  
+Checkbutton21 = tkinter.IntVar()  
+Checkbutton22 = tkinter.IntVar()  
+Checkbutton23 = tkinter.IntVar() 
+Checkbutton24 = tkinter.IntVar()  
+Checkbutton25 = tkinter.IntVar()  
+Checkbutton26 = tkinter.IntVar()  
+Checkbutton27 = tkinter.IntVar()  
+Checkbutton28 = tkinter.IntVar() 
+Checkbutton29 = tkinter.IntVar()  
+Checkbutton30 = tkinter.IntVar()  
+Checkbutton31 = tkinter.IntVar()  
+Checkbutton32 = tkinter.IntVar()  
+CheckbuttonCavity = tkinter.IntVar()  
+CheckbuttonCrown = tkinter.IntVar()  
+CheckbuttonRestoration = tkinter.IntVar()  
+CheckbuttonEnamel = tkinter.IntVar()  
+
 
 opList = []
 
 def checker():
 	if Checkbutton1.get() == 1:
-		if "Cavity" not in opList:
-			opList.append("Cavity")
+		if "1" not in opList:
+			opList.append("1")
 	else:
-		if "Cavity" in opList:
-			opList.remove("Cavity")
+		if "1" in opList:
+			opList.remove("1")
 
 	if Checkbutton2.get() == 1:
-		if "Wisdom" not in opList:
-			opList.append("Wisdom")
+		if "2" not in opList:
+			opList.append("2")
 	else:
-		if "Wisdom" in opList:
-			opList.remove("Wisdom")
+		if "2" in opList:
+			opList.remove("2")
 
 	if Checkbutton3.get() == 1:
-		if "Molar" not in opList:
-			opList.append("Molar")
+		if "3" not in opList:
+			opList.append("3")
 	else:
-		if "Molar" in opList:
-			opList.remove("Molar")
+		if "3" in opList:
+			opList.remove("3")
+
+	if Checkbutton4.get() == 1:
+		if "4" not in opList:
+			opList.append("4")
+	else:
+		if "4" in opList:
+			opList.remove("4")
+
+	if Checkbutton5.get() == 1:
+		if "5" not in opList:
+			opList.append("5")
+	else:
+		if "5" in opList:
+			opList.remove("5")
+	if Checkbutton6.get() == 1:
+		if "6" not in opList:
+			opList.append("6")
+	else:
+		if "6" in opList:
+			opList.remove("6")
+
+	if Checkbutton7.get() == 1:
+		if "7" not in opList:
+			opList.append("7")
+	else:
+		if "7" in opList:
+			opList.remove("7")
+
+	if Checkbutton8.get() == 1:
+		if "8" not in opList:
+			opList.append("8")
+	else:
+		if "8" in opList:
+			opList.remove("8")
+
+	if Checkbutton9.get() == 1:
+		if "9" not in opList:
+			opList.append("9")
+	else:
+		if "9" in opList:
+			opList.remove("9")
+
+	if Checkbutton10.get() == 1:
+		if "10" not in opList:
+			opList.append("10")
+	else:
+		if "10" in opList:
+			opList.remove("10")		
 
 	if Checkbutton11.get() == 1:
-		if "Etc" not in opList:
-			opList.append("Etc")
+		if "11" not in opList:
+			opList.append("11")
 	else:
-		if "Etc" in opList:
-			opList.remove("Etc")
+		if "11" in opList:
+			opList.remove("11")
 
 	if Checkbutton12.get() == 1:
-		if "Etc.." not in opList:
-			opList.append("Etc...")
+		if "12" not in opList:
+			opList.append("12")
 	else:
-		if "Etc.." in opList:
-			opList.remove("Etc...")
+		if "12" in opList:
+			opList.remove("12")
+
+	if Checkbutton13.get() == 1:
+		if "13" not in opList:
+			opList.append("13")
+	else:
+		if "13" in opList:
+			opList.remove("13")
+
+	if Checkbutton14.get() == 1:
+		if "14" not in opList:
+			opList.append("14")
+	else:
+		if "14" in opList:
+			opList.remove("14")
+
+	if Checkbutton15.get() == 1:
+		if "15" not in opList:
+			opList.append("15")
+	else:
+		if "15" in opList:
+			opList.remove("15")
+
+	if Checkbutton16.get() == 1:
+		if "16" not in opList:
+			opList.append("16")
+	else:
+		if "16" in opList:
+			opList.remove("16")
+
+	if Checkbutton17.get() == 1:
+		if "17" not in opList:
+			opList.append("17")
+	else:
+		if "17" in opList:
+			opList.remove("17")
+
+	if Checkbutton18.get() == 1:
+		if "18" not in opList:
+			opList.append("18")
+	else:
+		if "18" in opList:
+			opList.remove("18")
+
+	if Checkbutton19.get() == 1:
+		if "19" not in opList:
+			opList.append("19")
+	else:
+		if "19" in opList:
+			opList.remove("19")
+
+	if Checkbutton20.get() == 1:
+		if "20" not in opList:
+			opList.append("20")
+	else:
+		if "20" in opList:
+			opList.remove("20")
+
+	if Checkbutton21.get() == 1:
+		if "21" not in opList:
+			opList.append("21")
+	else:
+		if "21" in opList:
+			opList.remove("21")
+
+	if Checkbutton22.get() == 1:
+		if "22" not in opList:
+			opList.append("22")
+	else:
+		if "22" in opList:
+			opList.remove("22")
+
+	if Checkbutton23.get() == 1:
+		if "23" not in opList:
+			opList.append("23")
+	else:
+		if "23" in opList:
+			opList.remove("23")
+
+	if Checkbutton24.get() == 1:
+		if "24" not in opList:
+			opList.append("24")
+	else:
+		if "24" in opList:
+			opList.remove("24")
+
+	if Checkbutton23.get() == 1:
+		if "23" not in opList:
+			opList.append("23")
+	else:
+		if "23" in opList:
+			opList.remove("23")
+
+	if Checkbutton24.get() == 1:
+		if "24" not in opList:
+			opList.append("24")
+	else:
+		if "24" in opList:
+			opList.remove("24")
+
+	if Checkbutton25.get() == 1:
+		if "25" not in opList:
+			opList.append("25")
+	else:
+		if "25" in opList:
+			opList.remove("25")
+
+	if Checkbutton26.get() == 1:
+		if "26" not in opList:
+			opList.append("26")
+	else:
+		if "26" in opList:
+			opList.remove("26")
+
+	if Checkbutton27.get() == 1:
+		if "27" not in opList:
+			opList.append("27")
+	else:
+		if "27" in opList:
+			opList.remove("27")
+
+	if Checkbutton28.get() == 1:
+		if "28" not in opList:
+			opList.append("28")
+	else:
+		if "28" in opList:
+			opList.remove("28")
+
+	if Checkbutton29.get() == 1:
+		if "29" not in opList:
+			opList.append("29")
+	else:
+		if "29" in opList:
+			opList.remove("29")
+
+	if Checkbutton30.get() == 1:
+		if "30" not in opList:
+			opList.append("30")
+	else:
+		if "30" in opList:
+			opList.remove("30")
+
+	if Checkbutton31.get() == 1:
+		if "31" not in opList:
+			opList.append("31")
+	else:
+		if "31" in opList:
+			opList.remove("31")
+
+	if Checkbutton32.get() == 1:
+		if "32" not in opList:
+			opList.append("32")
+	else:
+		if "32" in opList:
+			opList.remove("32")
+
+	if CheckbuttonCavity.get() == 1:
+		if "cavity" not in opList:
+			opList.append("cavity")
+	else:
+		if "cavity" in opList:
+			opList.remove("cavity")
+
+	if CheckbuttonCrown.get() == 1:
+		if "crown" not in opList:
+			opList.append("crown")
+	else:
+		if "crown" in opList:
+			opList.remove("crown")
+
+	if CheckbuttonRestoration.get() == 1:
+		if "restoration" not in opList:
+			opList.append("restoration")
+	else:
+		if "restoration" in opList:
+			opList.remove("restoration")
+
+	if CheckbuttonEnamel.get() == 1:
+		if "enamel" not in opList:
+			opList.append("enamel")
+	else:
+		if "enamel" in opList:
+			opList.remove("enamel")
 
 def goToImages():
 	opWindow.destroy()
@@ -60,54 +324,153 @@ def goToImages():
 	for x in range(len(opList)):
 		print(opList[x])
 
-Button1 = tkinter.Checkbutton(opWindow, text = "Cavity", 
-                      variable = Checkbutton1,
-                      onvalue = 1,
-                      offvalue = 0,
-                      height = 2,
-                      width = 10,
-                      command=checker)
+Button1 = tkinter.Checkbutton(opWindow, text = "1", variable = Checkbutton1, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
 
-Button2 = tkinter.Checkbutton(opWindow, text = "Wisdom",
-                      variable = Checkbutton2,
-                      onvalue = 1,
-                      offvalue = 0,
-                      height = 2,
-                      width = 10,
-                      command=checker)
+Button2 = tkinter.Checkbutton(opWindow, text = "2", variable = Checkbutton2, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
 	  
-Button3 = tkinter.Checkbutton(opWindow, text = "Molar",
-                      variable = Checkbutton3,
-                      onvalue = 1,
-                      offvalue = 0,
-                      height = 2,
-                      width = 10,
-                      command=checker)  
+Button3 = tkinter.Checkbutton(opWindow, text = "3", variable = Checkbutton3, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker) 
 
-Button11 = tkinter.Checkbutton(opWindow, text = "Etc", 
-                      variable = Checkbutton11,
-                      onvalue = 1,
-                      offvalue = 0,
-                      height = 2,
-                      width = 10,
-                      command=checker)
+Button4 = tkinter.Checkbutton(opWindow, text = "4", variable = Checkbutton4, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
 
-Button12 = tkinter.Checkbutton(opWindow, text = "Etc...", 
-                      variable = Checkbutton12,
-                      onvalue = 1,
-                      offvalue = 0,
-                      height = 2,
-                      width = 10,
-                      command=checker)
+Button5 = tkinter.Checkbutton(opWindow, text = "5", variable = Checkbutton5, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
 
-Button4=tkinter.Button(opWindow, text="Submit", command = goToImages, bg = 'gainsboro') 
+Button6 = tkinter.Checkbutton(opWindow, text = "6", variable = Checkbutton6, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
+
+Button7 = tkinter.Checkbutton(opWindow, text = "7", variable = Checkbutton7, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
+	  
+Button8 = tkinter.Checkbutton(opWindow, text = "8", variable = Checkbutton8, onvalue = 1,
+                    		  offvalue = 0, height = 1, width = 10, command=checker) 
+
+Button9 = tkinter.Checkbutton(opWindow, text = "9", variable = Checkbutton9, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
+
+Button10 = tkinter.Checkbutton(opWindow, text = "10", variable = Checkbutton10, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
+
+Button11 = tkinter.Checkbutton(opWindow, text = "11", variable = Checkbutton11, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
+
+Button12 = tkinter.Checkbutton(opWindow, text = "12", variable = Checkbutton12, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
+	  
+Button13 = tkinter.Checkbutton(opWindow, text = "13", variable = Checkbutton13, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker) 
+
+Button14 = tkinter.Checkbutton(opWindow, text = "14", variable = Checkbutton14, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
+
+Button15 = tkinter.Checkbutton(opWindow, text = "15", variable = Checkbutton15, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
+
+Button16 = tkinter.Checkbutton(opWindow, text = "16", variable = Checkbutton16, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
+
+Button17 = tkinter.Checkbutton(opWindow, text = "17", variable = Checkbutton17, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
+	  
+Button18 = tkinter.Checkbutton(opWindow, text = "18", variable = Checkbutton18, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker) 
+
+Button19 = tkinter.Checkbutton(opWindow, text = "19", variable = Checkbutton19, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
+
+Button20= tkinter.Checkbutton(opWindow, text = "20", variable = Checkbutton20, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
+
+Button21 = tkinter.Checkbutton(opWindow, text = "21", variable = Checkbutton21, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
+
+Button22 = tkinter.Checkbutton(opWindow, text = "22", variable = Checkbutton22, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
+	  
+Button23 = tkinter.Checkbutton(opWindow, text = "23", variable = Checkbutton23, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker) 
+
+Button24 = tkinter.Checkbutton(opWindow, text = "24", variable = Checkbutton24, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
+
+Button25 = tkinter.Checkbutton(opWindow, text = "25", variable = Checkbutton25, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
+
+Button26 = tkinter.Checkbutton(opWindow, text = "26", variable = Checkbutton26, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
+
+Button27 = tkinter.Checkbutton(opWindow, text = "27", variable = Checkbutton27, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
+	  
+Button28 = tkinter.Checkbutton(opWindow, text = "28", variable = Checkbutton28, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker) 
+
+Button29 = tkinter.Checkbutton(opWindow, text = "29", variable = Checkbutton29, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
+
+Button30 = tkinter.Checkbutton(opWindow, text = "30", variable = Checkbutton30, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
+
+Button31 = tkinter.Checkbutton(opWindow, text = "31", variable = Checkbutton31, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
+
+Button32 = tkinter.Checkbutton(opWindow, text = "32", variable = Checkbutton32, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
+	  
+ButtonCavity = tkinter.Checkbutton(opWindow, text = "Cavity", variable = CheckbuttonCavity, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker) 
+
+ButtonCrown = tkinter.Checkbutton(opWindow, text = "Crown", variable = CheckbuttonCrown, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
+
+ButtonRestoration = tkinter.Checkbutton(opWindow, text = "Restoration", variable = CheckbuttonRestoration, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
+
+ButtonEnamel = tkinter.Checkbutton(opWindow, text = "Enamel", variable = CheckbuttonEnamel, onvalue = 1,
+                      		  offvalue = 0, height = 1, width = 10, command=checker)
+
+ButtonSubmit=tkinter.Button(opWindow, text="SUBMIT", command = goToImages, bg = 'gainsboro') 
     
-Button1.pack()  
-Button2.pack()  
-Button3.pack()
-Button11.pack()  
-Button12.pack()
-Button4.pack()	
+Button1.grid(row=1, column=1)
+Button2.grid(row=1, column=2)  
+Button3.grid(row=2, column=1)
+Button4.grid(row=2, column=2)  
+Button5.grid(row=3, column=1)  
+Button6.grid(row=3, column=2)
+Button7.grid(row=4, column=1)
+Button8.grid(row=4, column=2)
+Button9.grid(row=5, column=1)
+Button10.grid(row=5, column=2) 
+Button11.grid(row=6, column=1)
+Button12.grid(row=6, column=2)
+Button13.grid(row=7, column=1)
+Button14.grid(row=7, column=2)  
+Button15.grid(row=8, column=1)
+Button16.grid(row=8, column=2)
+Button17.grid(row=9, column=1)
+Button18.grid(row=9, column=2)
+Button19.grid(row=10, column=1)  
+Button20.grid(row=10, column=2)
+Button21.grid(row=11, column=1)
+Button22.grid(row=11, column=2) 
+Button23.grid(row=12, column=1) 
+Button24.grid(row=12, column=2)
+Button25.grid(row=13, column=1)
+Button26.grid(row=13, column=2)
+Button27.grid(row=14, column=1)
+Button28.grid(row=14, column=2)  
+Button29.grid(row=15, column=1)
+Button30.grid(row=15, column=2)
+Button31.grid(row=16, column=1)
+Button32.grid(row=16, column=2)
+ButtoneCavity.grid(row=17, column=1)
+ButtonCrown.grid(row=17, column=2)
+ButtonRestoration.grid(row=18, column=1)
+ButtonEnamel.grid(row=18, column=2)
+ButtonSubmit.grid(row=19, column=1, sticky="")
 
 
 imWindow = Toplevel(master)
@@ -117,7 +480,7 @@ imWindow.configure(background = 'black')
 l = tkinter.Label(imWindow, text = "Welcome!\n Here you can sift thru images as you please!",
 		              fg = 'aquamarine2', bg = 'black', bd = 0)
 l.config(font=("Courier", 18, "bold"))
-l.pack(padx = 1, pady = 1)
+l.place(x=425,y=50,anchor="center")
 
 master.withdraw()
 imWindow.withdraw()
