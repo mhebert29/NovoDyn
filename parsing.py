e435476ae7b6ea8afecdb415008a4fcc364beadf
dawstheboss26 <78572624+dawstheboss26@users.noreply.github.com>
2021-05-11 15:10:33 -0400

    Update parsing.py

--- parsing.py
+++ parsing.py
@@ -1,27 +1,34 @@
 #!/usr/bin/env python3
-
+# parsing.py
+# Created by Dawson Flatley to work with Madeline Herbert's "gui.py" and Raul Amezcua's DLList sorting
+# Part of team NovoDynamics project 
 import sys
 import os
 import json
 import pathlib
 from pathlib import Path
 
-
+# findtags
+# Inputs: 
+#	folder: path of folder with all VoTT .json files
+#	tags: list of tags to look for (photo must include all specified tags)
+# Outputs:
+#	outlist: a dictionary in which the number of regions as a key and filename of photo as the value
 def findtags(folder,tags):
-	outlist = {}
-	filteredtags = {}
-	for filename in os.listdir(folder):
-		for tag in tags:
+	outlist = {} # Output dictionary
+	filteredtags = {} # Dictionary of booleans to indicate which tags are found
+	for filename in os.listdir(folder): #iterate through directory of jsons
+		for tag in tags: # Set each tags "found" bool to false
 			filteredtags[tag] = False;
-		mypath = folder + "/" + filename
+		mypath = folder + "/" + filename #build full path to the .json file
 		with open(mypath) as f:
 			data = json.load(f)
 			for region in data["regions"]:
 				for intag in region["tags"]:
-					if intag in filteredtags:
+					if intag in filteredtags: # Check if tag is one of user-specified tags
 						filteredtags[intag] = True
-		if all(filteredtags.values()):
-			outlist[len(data["regions"])] = data["asset"]["name"]
+		if all(filteredtags.values()): # Needs to have found all tags
+			outlist[len(data["regions"])] = data["asset"]["name"] #Key: number of regions, Value: name of image file
 	return outlist
 			
 if __name__ == "__main__":
